e4e63eeecf949cd9797e9728ec74aaf419405b57
tmikdashi <tmikdashi@gmail.com>
2023-09-26 13:50:12 -0400

    Updated to address comments

--- analyses/export_plot_county_data.py
+++ analyses/export_plot_county_data.py
@@ -1,12 +1,8 @@
 import csv
 import os
 from collections import defaultdict
-
-import matplotlib.pyplot as plt
 import numpy as np
 from deampy.in_out_functions import write_csv
-from deampy.plots.plot_support import output_figure
-
 from definitions import ROOT_DIR
 
 # csv filename where the data is located
@@ -32,17 +28,20 @@ with open(csv_file_path, 'r', newline='') as csvfile:
         if state == 'NA':
             state = 'PR'
         # Replace missing values with 'NA'
-        cases = cases if cases != '' else 'NA'
+        cases = float(cases) if cases != '' else np.nan
 
         # Append the data to the respective county's time series
-        # TODO: note that country is not a unique identifier (there are multiple counties with the same name).
-        #  So here you need to use the state as well to uniquely identify the county.
+        # TODO: CHANGE COMPLETE, note that country is not a unique identifier (there are multiple counties with the
+        #  same name).So here you need to use the state as well to uniquely identify the county.
         #  For example, you can use the state abbreviation and county name as a tuple.
         #   county_cases_time_series[(county, state)].append((date, cases))
-        county_cases_time_series[county].append((date, cases, state))
+        county_cases_time_series[(county,state)].append((date, cases))
 
 # Create a list of unique dates across all counties
-all_dates = sorted(set(date for time_series in county_cases_time_series.values() for date, _, _ in time_series))
+all_dates = sorted(set(date for time_series in county_cases_time_series.values() for date, _ in time_series))
+
+# Create a list of unique county and state combinations
+unique_county_state_combinations = sorted(set((county, state) for county, state in county_cases_time_series.keys()))
 
 # Create the header row with dates
 header_row = ['County', 'State'] + all_dates
@@ -50,66 +49,22 @@ header_row = ['County', 'State'] + all_dates
 # Create a list of data rows for each county, ensuring length of all the rows is the same
 county_cases_rows = []
 
-for county, time_series in county_cases_time_series.items():
+for county, state in unique_county_state_combinations:
+    time_series = county_cases_time_series[(county, state)]
     data = []
     for date in all_dates:
-        for time_date, time_cases, time_state in time_series:
+        found = False
+        for time_date, time_cases in time_series:
             if time_date == date:
                 data.append(time_cases)
+                found = True
                 break
-        else:
-            #If data is missing for a date, fill with 'NA'
-            # TODO: it would be better to use math.nan or np.nan instead of 'NA' to represent missing data.
-            data.append('NA')
-    county_cases_rows.append([county, time_series[0][2]] + data)
+        if not found:
+            # If data is missing for a date, fill with np.nan
+            data.append(np.nan)
+    county_cases_rows.append([county, state] + data)
 
 # Write into a CSV file using the write_csv function
 write_csv(rows=[header_row] + county_cases_rows, file_name=ROOT_DIR + '/data/summary/county_cases.csv')
 
 
-# TODO: Could you please move the code below to a separate file called plot_county_data.py?
-
-# PART 2
-
-# Get the first 9 counties from county_cases_time_series
-# TODO: I think it makes sense to only look at a limited number of counties at this stage
-#  (there are more 3000 counties in the US I think!). However, let's make sure to include
-#  counties with missing observations as well to see if the code is working properly.
-counties_to_plot = list(county_cases_time_series.keys())[:9]
-
-# Define a marker for missing data points
-missing_data_marker = 'x'
-
-# Loop through the selected counties
-for county in counties_to_plot:
-    # Extract the date and cases data for the county
-    time_series = county_cases_time_series[county]
-    dates = [date for date, _, _ in time_series]
-    cases = [float(cases) if cases != 'NA' else np.nan for _, cases, _ in time_series]
-
-    # Create a new figure and plot the data
-    plt.figure(figsize=(12, 6))  # Adjust the figure size as needed
-    # TODO: I am not sure what markevery does. Could you please explain?
-    #   Also, I think if plot function encounters a nan value, it will not plot anything for that date (which is good).
-    #   In other word, the line will be broken for dates with missing data.
-    plt.plot(dates, cases, label=f'{county}, {time_series[0][2]}',
-             marker=missing_data_marker, markevery=[i for i, c in enumerate(cases) if np.isnan(c)])
-    plt.xlabel('Date')
-    plt.ylabel('Number of Cases')
-    plt.title(f'Cases Time Series for {county}, {time_series[0][2]}')
-    plt.xticks(rotation=90, fontsize=6)  # Rotate x-axis labels for better visibility
-    plt.legend()
-
-    # For dates with missing data, the date is labeled in red
-    # TODO: if the line could be broken for dates with missing data, we probably don't need to do this.
-    for i, case in enumerate(cases):
-        if np.isnan(case):
-            plt.text(dates[i], 0, dates[i], color='red', verticalalignment='bottom', horizontalalignment='center')
-
-    # Save each plot with a unique filename, e.g., county_cases_countyname.png
-    filename = os.path.join(ROOT_DIR + '/data/summary', f'county_cases_{county}.png')
-
-    output_figure(plt, filename)
-
-# Show the plots (optional)
-plt.show()
--- analyses/plot_county_data.py
+++ analyses/plot_county_data.py
@@ -1,5 +1,71 @@
-
-
 # TODO: please move the code to plot cases by county here.
 #  It should read the csv file county_cases.csv and plot the cases
 
+import csv
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+from deampy.plots.plot_support import output_figure
+from definitions import ROOT_DIR
+
+# PART 1: Loading the data for plotting
+
+csv_file_path = ROOT_DIR + '/analyses/Users/timamikdashi/PycharmProjects/covid19-qaly-loss/data/summary/county_cases.csv'
+
+# Create a dictionary to store the data from the CSV file
+county_cases_data = {}
+
+# Open and read the CSV file
+with open(csv_file_path, 'r', newline='') as csvfile:
+    csvreader = csv.reader(csvfile)
+    header = next(csvreader)
+
+    for row in csvreader:
+        county = row[0]
+        state = row[1]
+        cases = row[2:]
+
+        # Convert cases to a list of floats, handling missing values
+        cases = [float(case) if case != 'NA' else np.nan for case in cases]
+
+        county_cases_data[(county, state)] = cases
+
+# PART 2: Finding counties and states with missing values and counting missing values
+counties_with_missing_values = {}
+
+for (county, state), cases in county_cases_data.items():
+    num_missing_values = sum(np.isnan(cases))
+    if num_missing_values > 0:
+        counties_with_missing_values[(county, state)] = num_missing_values
+
+
+# PART 3: PLOTTING the first 9 counties with missing data
+counties_to_plot = list(counties_with_missing_values.keys())[:9]
+
+# Loop through counties to plot
+for (county, state) in counties_to_plot:
+    # Extract the date and cases data for the county
+    cases = county_cases_data[(county, state)]
+    dates = header[2:]
+
+    # Creating a new figure and plot the data
+    plt.figure(figsize=(12, 6))
+
+    # Plotting available data as blue circles for contrast
+    plt.plot(dates, cases, label=f'{county}, {state}', marker='o', linestyle='-', markersize=4, linewidth=1.5, color='blue')
+
+    # For missing data, we will convert NaN to 0 and plot it as a red 'x'
+    missing_data_marker = {'marker': 'x', 'color': 'red'}
+    for i, is_missing in enumerate(cases):
+        if np.isnan(is_missing):
+            plt.scatter(dates[i], 0, marker='x', color='red')
+
+    plt.xlabel('Date')
+    plt.ylabel('Number of Cases')
+    plt.title(f'Cases Time Series for {county}, {state}')
+    plt.xticks(rotation=90, fontsize=6)  # Rotate x-axis labels for better visibility
+    plt.legend()
+
+    # Save each plot with a unique filename, e.g., county_cases_countyname.png
+    filename = os.path.join(ROOT_DIR + '/data/summary', f'county_cases_{county}_{state}.png')
+    output_figure(plt, filename)
