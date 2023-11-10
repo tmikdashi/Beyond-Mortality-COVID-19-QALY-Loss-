b28b6dfe57d5f4bb8a59a23d2d0d6f28d9b9000a
rezayas <reza.yaesoubi@yale.edu>
2023-11-10 16:15:50 -0500

    reza's edits

--- tests/generate_data.py
+++ tests/generate_data.py
@@ -1,6 +1,6 @@
 from data_preprocessing.support_functions import generate_county_data_csv
 
-# TODO: this could be run once so I moved it to a seperate file so that we don't have to run it
+# TODO: this could be run once so I moved it to a separate file so that we don't have to run it
 #  every time we run the tests
 
 # Create the model and populate it with data from the provided csv files
